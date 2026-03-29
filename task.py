e5041c84b7b706e8a7249ceacef02bff533e57f6
Adelina20034 <sat_ade@mail.ru>
2026-03-29 17:08:23 +0300

    auto: update solution for 69a069f9e483d40a8c785cd4

--- task.py
+++ task.py
@@ -4,7 +4,8 @@ from langchain.tools import BaseTool, StructuredTool
 from langchain.schema.messages import HumanMessage, SystemMessage
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.callbacks.manager import CallbackManagerForToolRun
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.stdout_callback_handler import StdOutCallbackHandler
 from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
 
@@ -14,9 +15,8 @@ class GetPriceTool(BaseTool):
         "Используется для получения цены конкретного товара в конкретном городе."
     )
 
-    def _run(self, product: str, city: str, run_manager: CallbackManagerForToolRun = None) -> str:
-        sub_llm = ChatOpenAI(model='gpt-3.5-turbo', base_url='http://localhost:1234/v1', api_key=SecretStr('fake'), temperature=0.7)
+    def _run(self, product: str, city: str) -> str:
         prompt_template = """
             Ты эксперт по ценам товаров в разных городах России.
             Назови среднюю цену {product} в городе {city}.
-            Форматируй ответ в виде таблицы Markdown со столбцами 'Продукт', 'Цена (руб.)', 'Магазин':
+            Форматируй ответ в виде таблицы Markdown:
